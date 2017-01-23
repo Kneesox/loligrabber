1c54bf52acc78a76fccb2bf343dd4d4756dceab5
Shiro <ShiroNullBlank@users.noreply.github.com>
2017-01-23 16:07:29 +0000

    Create app.js
    The downloader

---
+++ app.js
@@ -0,0 +1,13 @@
+var downloadImage = require('download-image');
+var request = require('request');
+var url = 'http://lolwaleet.com/Loli/';
+
+function lolis() {
+request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    downloadImage('http://i.imgur.com/'+ body.substring(1359,1370), __dirname+'\\loli\\'+body.substring(1359,1370));
+		}
+	})
+}
+setInterval(lolis, 1000);
+console.log("Made by Kneesox#0461")
